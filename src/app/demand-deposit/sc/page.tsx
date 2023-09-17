c5bd7b926580159cf9dbf181bf2fdfb7166346c7
niskan516 <sinhong2011@gmail.com>
2023-09-17 15:56:51 +0800

    feat: add sc page and update header menu

---
+++ src/app/demand-deposit/sc/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: '渣打高息馬拉松活期存款',
+}
+
+export default function ScPage() {
+  return <div></div>
+}
--- src/components/layouts/header-menu.tsx
+++ src/components/layouts/header-menu.tsx
@@ -12,16 +12,25 @@ import {
   NavigationMenuViewport,
 } from '@/components/ui/navigation-menu'
 import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu'
+import React from 'react'
+import { cn } from '@/lib/utils'
 
 const navList = [
   {
-    name: '定期存款',
+    title: '定期存款',
     href: '/',
   },
   {
-    name: '活期存款',
+    title: '活期存款',
     href: '/demand-deposit',
-    subMenu: [],
+    subMenu: [
+      {
+        title: '渣打高息馬拉松活期存款',
+        href: '/demand-deposit/sc',
+        description: '活期存款預測表',
+      },
+      {},
+    ],
   },
 ]
 
@@ -38,13 +47,34 @@ export default function HeaderMenu() {
             {navList.map((navItem, navIdx) => {
               return (
                 <NavigationMenuItem key={navIdx}>
-                  <Link href={navItem.href} legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      {navItem.name}
-                    </NavigationMenuLink>
-                  </Link>
+                  {navItem.subMenu ? (
+                    <>
+                      <NavigationMenuTrigger>
+                        {navItem.title}
+                      </NavigationMenuTrigger>
+                      <NavigationMenuContent>
+                        <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] '>
+                          {navItem.subMenu.map((component, cIdx) => (
+                            <ListItem
+                              key={cIdx}
+                              title={component.title}
+                              href={component.href}
+                            >
+                              {component.description}
+                            </ListItem>
+                          ))}
+                        </ul>
+                      </NavigationMenuContent>
+                    </>
+                  ) : (
+                    <Link href={navItem.href} legacyBehavior passHref>
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        {navItem.title}
+                      </NavigationMenuLink>
+                    </Link>
+                  )}
                 </NavigationMenuItem>
               )
             })}
@@ -54,3 +84,29 @@ export default function HeaderMenu() {
     </header>
   )
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className
+          )}
+          {...props}
+        >
+          <div className='text-sm font-medium leading-none'>{title}</div>
+          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = 'ListItem'
