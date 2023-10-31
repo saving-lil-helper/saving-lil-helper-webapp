35d93eafef49d2c9cb7f91e5a042d19e4a3bf375
niskan516 <sinhong2011@gmail.com>
2023-10-31 21:01:32 +0800

    fix: fix non-supported feature

--- src/app/demand-deposit/sc/ScRateHistory.tsx
+++ src/app/demand-deposit/sc/ScRateHistory.tsx
@@ -10,34 +10,37 @@ export default function ScRateHistory() {
     <div className='mt-6 h-72 w-full rounded-md border p-7'>
       <h2 className='text-3xl font-bold'>已記錄歷史推廣息率</h2>
       <ScrollArea className='mt-5'>
-        {scRateData.data.toReversed().map((item, index) => (
-          <>
-            <div key={index} className='flex justify-between'>
-              <span className='text-md'>
-                {format(
-                  new Date(
-                    item.promotion_date.year,
-                    item.promotion_date.month - 1
-                  ),
-                  'yyyy/MM'
-                )}
-              </span>
-              <div className='flex items-center space-x-4 text-sm'>
-                {item.phases.map((phase, pIndex) => (
-                  <>
-                    <div key={pIndex} className='w-8 text-center'>
-                      {phase.rate}%
-                    </div>
-                    {pIndex !== item.phases.length - 1 && (
-                      <Separator orientation='vertical' />
-                    )}
-                  </>
-                ))}
+        {scRateData.data
+          .slice()
+          .reverse()
+          .map((item, index) => (
+            <>
+              <div key={index} className='flex justify-between'>
+                <span className='text-md'>
+                  {format(
+                    new Date(
+                      item.promotion_date.year,
+                      item.promotion_date.month - 1
+                    ),
+                    'yyyy/MM'
+                  )}
+                </span>
+                <div className='flex items-center space-x-4 text-sm'>
+                  {item.phases.map((phase, pIndex) => (
+                    <>
+                      <div key={pIndex} className='w-8 text-center'>
+                        {phase.rate}%
+                      </div>
+                      {pIndex !== item.phases.length - 1 && (
+                        <Separator orientation='vertical' />
+                      )}
+                    </>
+                  ))}
+                </div>
               </div>
-            </div>
-            <Separator className='my-2' />
-          </>
-        ))}
+              <Separator className='my-2' />
+            </>
+          ))}
       </ScrollArea>
     </div>
   )
